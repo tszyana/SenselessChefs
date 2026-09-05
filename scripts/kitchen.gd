e3f2b3afd675ea00f1597523fa91ca5f398c3142
Emmy <emh06@students.cs.ubc.ca>
2026-09-05 12:03:32 -0700

    Pause button added

--- scripts/kitchen.gd
+++ scripts/kitchen.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 @onready var game_timer = $Timer
 @onready var time_label = $Control/TimerLabel
-@onready var panel_popup = $Control/TimerPopup
+@onready var timer_popup = $Control/TimerPopup
+@onready var pause_popup = $Control/PausePanel
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,4 +17,22 @@ func _process(_delta) -> void:
 
 
 func _on_timer_timeout() -> void:
-	panel_popup.visible = true
+	timer_popup.visible = true
+
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/kitchen.tscn")
+
+
+func _on_pause_button_pressed() -> void:
+	get_tree().paused = true
+	pause_popup.visible = true
+
+
+func _on_resume_button_pressed() -> void:
+	get_tree().paused = false
+	pause_popup.visible = false
