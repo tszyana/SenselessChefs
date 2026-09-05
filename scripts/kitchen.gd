94ed13859e93120dbc6f20d1a1483a957dcf7387
Emmy <emh06@students.cs.ubc.ca>
2026-09-05 11:22:25 -0700

    Added timer countdown

--- scripts/kitchen.gd
+++ scripts/kitchen.gd
@@ -1,12 +1,14 @@
-extends Label
+extends Node2D
 
+@onready var game_timer = $Timer
+@onready var time_label = $Control/TimerLabel
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-	
+func _process(_delta) -> void:
+	#pass
+	time_label.text = "Time left:\n" + str(int(game_timer.time_left))
