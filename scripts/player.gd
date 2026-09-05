cc0cbec1bbceb8fd7058bb59a08e36da9731e5a7
aisling lu <aislinglu2@gmail.com>
2026-09-04 20:28:42 -0700

    player move in 4 directions

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+enum State { MOVE, HOLD, CHOP, STIR}
+var state = State.MOVE
+
 func _physics_process(delta: float) -> void:
 	process_movement()
 	move_and_slide()
@@ -10,9 +13,17 @@ func _physics_process(delta: float) -> void:
 func process_movement() -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
+	var direction := Input.get_vector("left", "right", "up", "down")
 	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity = direction * SPEED
+	
+	update_sprite(direction)
+
+func update_sprite(dir: Vector2) -> void:
+	if dir.x != 0:
+		pass # sprite.flip_h = dir.x < 0 to flip sprite left/right
+	elif dir.y < 0: 
+		pass # change sprite to up
+	elif dir.y > 0:
+		pass # change sprite
+		
