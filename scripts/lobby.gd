25522adec7a9026f54224181b722c21b2c63cc97
Emmy <emh06@students.cs.ubc.ca>
2026-09-05 12:30:57 -0700

    Did some lobby ui work

--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -108,3 +108,7 @@ func set_ready_on_server(ready: bool) -> void:
 func start_game() -> void:
 	get_tree().change_scene_to_file("res://scenes/mic_test.tscn")
 	
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
