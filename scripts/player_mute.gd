c5178f4e776e03ed1877e733e57873b3c7b0a387
aisling lu <aislinglu2@gmail.com>
2026-09-04 23:34:30 -0700

    added outline code for mute players gestures

--- scripts/player_mute.gd
+++ scripts/player_mute.gd
@@ -1,11 +1,27 @@
 # mute player
 extends PlayerMovement
 
-enum State {}
+enum State { POINT, HAPPY, MAD, ONE, TWO, THREE, FOUR }
+var state = null
 
 func _physics_process(delta: float) -> void:
 	process_movement()
 	move_and_slide()
+	update_gesture_sprite()
 
 func update_gesture_sprite() -> void:
-	pass
+	match state:
+		State.POINT:
+			pass
+		State.HAPPY:
+			pass
+		State.MAD:
+			pass
+		State.ONE:
+			pass
+		State.TWO:
+			pass
+		State.THREE:
+			pass
+		State.FOUR:
+			pass
