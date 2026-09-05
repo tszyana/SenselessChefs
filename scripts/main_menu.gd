eca396632f32384d42bd7dc083a06a99483c3030
Gina Liu <ginatyliu@gmail.com>
2026-09-04 20:48:56 -0700

    switches to lobby after pressing host button

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,6 +4,7 @@ extends Control
 func _on_host_button_pressed() -> void:
 	print("HOST BUTTON PRESSED")
 	NetworkManager.host_game()
+	get_tree().change_scene_to_file("res://scenes/lobby.tscn")
 	
 func _on_join_button_pressed() -> void:
 	print("JOIN GAME BUTTON PRESSED")
