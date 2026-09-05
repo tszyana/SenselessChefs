110e21572aa53ae3b773ce953c21c4e9632420d1
Emmy <emh06@students.cs.ubc.ca>
2026-09-05 10:51:04 -0700

    Added trashcan

--- scripts/timer_label.gd
+++ scripts/timer_label.gd
@@ -1,8 +1,5 @@
 extends Label
 
-#@onready var game_timer = $GameTimer
-#@onready var time_label = $CanvasLayer/TimeLabel
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,7 +7,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#time_label.text = "Time: " + str(ceil(game_timer.time_left))
 	pass
 	
 	
