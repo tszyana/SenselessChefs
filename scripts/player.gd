62584cee04b983c8b002db0eae85b87e1c1b5381
aisling lu <aislinglu2@gmail.com>
2026-09-04 20:41:53 -0700

    player move input problem fixed

--- scripts/player.gd
+++ scripts/player.gd
@@ -20,10 +20,18 @@ func process_movement() -> void:
 	update_sprite(direction)
 
 func update_sprite(dir: Vector2) -> void:
-	if dir.x != 0:
-		pass # sprite.flip_h = dir.x < 0 to flip sprite left/right
-	elif dir.y < 0: 
-		pass # change sprite to up
-	elif dir.y > 0:
-		pass # change sprite
+	match state:
+		State.MOVE:
+			if dir.x != 0:
+				pass # sprite.flip_h = dir.x < 0 to flip sprite left/right
+			elif dir.y < 0: 
+				pass # change sprite to up
+			elif dir.y > 0:
+				pass # change sprite
+		State.HOLD:
+			pass
+		State.CHOP:
+			pass
+		State.STIR:
+			pass
 		
