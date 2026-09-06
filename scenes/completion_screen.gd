ee3e8c8629f52d2b5dee699ea8193f1b8e5b7585
Emmy <emh06@students.cs.ubc.ca>
2026-09-06 00:03:48 -0700

    Completion menu button

--- scenes/completion_screen.gd
+++ scenes/completion_screen.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_to_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
