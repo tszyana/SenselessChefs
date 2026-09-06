61d6d25df09f3bf50c0fddb741ca6f1a563c098d
aisling lu <aislinglu2@gmail.com>
2026-09-06 12:21:47 -0700

    starting blindness thing

---
+++ scripts/darkness.gd
@@ -0,0 +1,15 @@
+extends ColorRect
+
+@export var radius_px: float = 120.0
+@export var feather_px: float = 60.0
+
+var player: Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
