0784fbaf6206543cf64ebd5d6eaf5dd8d8e3ff58
aisling lu <aislinglu2@gmail.com>
2026-09-05 22:59:44 -0700

    adding flipping character sprites

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,6 +1,7 @@
 class_name PlayerMovement
 extends CharacterBody2D
 
+@onready var sprite: Sprite2D = $Sprite2D
 
 const SPEED = 500.0
 var can_move := true
@@ -24,9 +25,5 @@ func process_movement() -> void:
 # updates sprite to match movement
 func update_movement_sprite(dir: Vector2) -> void:
 	if dir.x != 0:
-		pass # sprite.flip_h = dir.x < 0 to flip sprite left/right
-	elif dir.y < 0: 
-		pass # change sprite to up
-	elif dir.y > 0:
-		pass # change sprite
+		sprite.flip_h = dir.x < 0 
 		
